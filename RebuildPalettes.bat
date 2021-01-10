905f13146ee9f1c4093213a39d33e03819eafba9
Cam Sinclair <csinkers@gmail.com>
2021-01-09 21:09:08 -0500

    Reorganise, add test saves and palette test

--- RebuildPalettes.bat
+++ RebuildPalettes.bat
@@ -2,17 +2,17 @@
     --size 64                         ^
     --offset 192                      ^
     --trim                            ^
-    --out Assets\PALETTE.000\0.raw    ^
-    --outbmp Assets\PALETTE.000\0.bmp ^
-    Assets\ITEMGFX                    ^
-    Assets\PARTGR0
+    --out Assets\Gfx\Common.pal       ^
+    --outbmp Assets\Gfx\CommonPal.bmp ^
+    Assets\Gfx\Item                   ^
+    Assets\Gfx\PartyLarge
 
 @dotnet run --project ..\..\src\Tools\PaletteBuilder -- ^
-    --size 191                      ^
-    --base Assets\PALETTE.000\0.raw ^
-    --baseoffset 192                ^
-    --out Assets\PALETTE0\0.raw     ^
-    --outbmp Assets\PALETTE0\0.bmp  ^
-    --images                        ^
-    Assets
+    --size 191                        ^
+    --base Assets\Gfx\Common.pal     ^
+    --baseoffset 192                  ^
+    --out Assets\Gfx\Palette\0.pal    ^
+    --outbmp Assets\Gfx\Palette\0.bmp ^
+    --images                          ^
+    Assets\Gfx
 
